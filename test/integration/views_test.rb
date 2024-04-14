dcf20ce6ef9688108cd33074d58d018c2fdefb88
hje <hjerikson@gmail.com>
2024-04-14 16:16:01 +0200

    Write integration tests for links, views, users

---
+++ test/integration/views_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class ViewsTest < ActionDispatch::IntegrationTest
+  # Check that when we click on an URL, view is created, count goes up, redirect 
+  test "viewing a link records a view" do
+    link = links(:one)
+    original_views_count = link.views_count
+
+    assert_difference "View.count" do
+      assert_difference "link.views_count" do
+        get view_path(link)
+        assert_response :redirect
+        link.reload
+      end
+    end
+  end
+
+  test "visiting a link redirects to the URL" do
+    link = links(:one)
+
+    get view_path(link)
+    assert_redirected_to link.url
+  end
+end
