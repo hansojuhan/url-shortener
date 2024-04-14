6a01fb688f115f75455b36c0116151e164c04c8c
hje <hjerikson@gmail.com>
2024-04-14 19:00:39 +0200

    Switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_14_132314) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.string "title"
@@ -39,7 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_14_132314) do
     t.string "ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "link_id", null: false
+    t.bigint "link_id", null: false
     t.index ["link_id"], name: "index_views_on_link_id"
   end
 
